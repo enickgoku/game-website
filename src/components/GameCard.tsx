9370f712b74d096ed84e49c61632ab81a58939a9
Nick Engelhardt <enickgoku2@gmail.com>
2023-05-07 13:53:31 -0400

    removed excess flex container

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,33 +11,26 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Flex
-      height="400px"
-      width="300px"
-      boxShadow="4px 4px 4px rgba(0, 0, 0, 0.25)"
-      borderRadius="8px 8px 8px 8px"
-    >
-      <Card height="100%" width="100%">
-        <Image
-          src={getCroppedImageUrl(game.background_image)}
-          maxHeight="220px"
-          objectFit="cover"
-          borderRadius={3}
-        />
-        <CardBody>
-          <HStack justifyContent="space-between" marginBottom={3}>
-            <PlatformList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-          <Heading fontSize="2xl">
-            {game.name}
-            <Emoji rating={game.rating_top} />
-          </Heading>
-        </CardBody>
-      </Card>
-    </Flex>
+    <Card height="100%" width="100%">
+      <Image
+        src={getCroppedImageUrl(game.background_image)}
+        maxHeight="220px"
+        objectFit="cover"
+        borderRadius={3}
+      />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
+      </CardBody>
+    </Card>
   );
 };
 
