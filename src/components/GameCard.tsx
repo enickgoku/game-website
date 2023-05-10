a13337836834a447b573da43441f2c18436992cb
Nick Engelhardt <enickgoku2@gmail.com>
2023-05-10 18:35:49 -0400

    refactoring

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
 import PlatformList from "./PlatformList";
 import CriticScore from "./CriticScore";
